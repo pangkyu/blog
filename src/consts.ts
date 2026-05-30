02dca7b14a8c56ec390b3239cd0c42318eff0699
Bae seong-kyu <baesense@naver.com>
2026-05-30 17:59:53 +0900

    Feat : 시리즈 연동 기능

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-export const SITE_TITLE = "개발 블로그";
-export const SITE_DESCRIPTION = "공부한 것들을 기록하는 개발 블로그입니다.";
+export const SITE_TITLE = "pangkyu.log";
+export const SITE_DESCRIPTION = "학습 및 개발 관련 글을 기록하는 블로그입니다.";
 export const SITE_AUTHOR = "배성규";
 export const SITE_EMAIL = "qo1374@gmail.com";
 export const GITHUB_URL = "https://github.com/pangkyu";
